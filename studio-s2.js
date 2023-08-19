6ffe1002e1cbf4cab9ba6a73be0aa9d9728dd618
Source Academy User <No public email provided>
2023-08-19 23:39:04 +0800

    fixed the problem where i cant see too well

---
+++ studio-s2.js
@@ -0,0 +1,38 @@
+
+
+function biggie_size(x){
+    return (x+4);
+}
+
+function unbiggie_size(x){
+    return (x-4);
+}
+
+function is_biggie_size(x){
+    return x >= 5;
+}
+
+function combo_price(x){
+    return is_biggie_size(x)
+        ? unbiggie_size(x) * 1.17 + 0.50
+        : x * 1.17;
+}
+
+function empty_order(){
+    return 0;
+}
+
+function add_to_order(x,y){
+    return x*10+y;
+}
+
+function last_combo(x){
+    return x % 10;
+}
+
+function other_combo(x){
+    return (x - last_combo(x))/10;
+    
+}
+
+
