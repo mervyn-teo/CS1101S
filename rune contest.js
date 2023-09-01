442ffe678f7de9420f070ce62e0caa2838a0b572
Source Academy User <No public email provided>
2023-09-01 16:16:09 +0800

    Changes made from Source Academy

--- rune contest.js
+++ rune contest.js
@@ -1,16 +1,68 @@
+import {
+blank,
+circle,
+corner,
+heart,
+nova,
+pentagram,
+rcross,
+ribbon,
+sail,
+square,
+triangle,
+turn_upside_down,
+black,
+blue,
+brown,
+color,
+green,
+indigo,
+orange,
+pink,
+purple,
+random_color,
+red,
+white,
+yellow,
+
+anaglyph,
+animate_anaglyph,
+animate_rune,
+beside,
+beside_frac,
+flip_horiz,
+flip_vert,
+from_url,
+hollusion,
+hollusion_magnitude,
+make_cross,
+overlay,
+overlay_frac,
+quarter_turn_left,
+quarter_turn_right,
+repeat_pattern,
+rotate,
+scale,
+scale_independent,
+show,
+stack,
+stack_frac,
+stackn,
+translate} from "rune";
+
 function helper(r, y, rune){
     return translate(math_sqrt(r * r - y * y), y, rune);
 }
 
-function stepper(y, steps, yend, r, rend, rune){
+function stepper(y, steps, yend, r, rune){
     overlay(helper(r, y, rune), rune);
-    return y >= yend || r>= rend
-        ? helper(rend, yend, rune)
-        : stepper(y + steps, steps, yend, r + steps, rend, rune);
+    return y >= yend
+        ? helper(r, yend, rune)
+        : stepper(y + steps, steps, yend, r , rune);
 }
 
 function runes_contest() {
-    return stepper(0, 0.1, 1, 0, 1, scale(0.1, circle));
+    return stepper(0, 0.1, 1, 0.5, scale(0.1, circle));
 } 
 
 // Keep this show function call
