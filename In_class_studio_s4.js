0ee7280d2d36c4f3fd33a492afe4a40f15b0129a
Source Academy User <No public email provided>
2023-09-11 15:21:22 +0800

    fun

---
+++ In_class_studio_s4.js
@@ -0,0 +1,31 @@
+function every_second(item){
+    return is_null(tail(item))
+        ? null
+        : is_null(tail(tail(item)))
+        ? pair(head(tail(item)), null)
+        : pair(head(tail(item)), every_second(tail(tail(item))));
+        
+}
+
+every_second(list(0,1,2,3,4,5,6,7));
+
+
+function sum(item){
+    function odd(item){
+        return is_null(tail(item))
+        ? 0 
+        : is_null(tail(tail(item)))
+        ? head(tail(item))
+        : head(tail(item)) + odd(tail(tail(item)));
+    }
+        function even(item){
+        return is_null(tail(item))
+        ? head(item)
+        : is_null(tail(tail(item)))
+        ? 0
+        : head(item) + even(tail(tail(item)));
+    }
+    return list(even(item), odd(item));
+}
+
+sum(list(0,1,2,3,4,5,6,7));
