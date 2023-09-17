54287b60b5bb62e585141c8b2a91e28a863fb087
Source Academy User <No public email provided>
2023-09-18 01:09:28 +0800

    Changes made from Source Academy

--- studio-s6.js
+++ studio-s6.js
@@ -1,6 +1,19 @@
+//Write the function map using accumulate. Name your function my_map.
 function my_map(f, xs) {
     return accumulate((x, y) => pair(f(x), y), null, xs);
 }
 //Example calls:
 my_map(x => x + 1, list(1, 2, 3));
 // Result: list(2, 3, 4)
+
+//Write a function called remove_duplicates that takes in a list as its only argument and
+// returns a list with duplicate elements removed. The order of the elements in the returned
+// list does not matter. Use filter in your function.
+function remove_duplicates(lst) {
+    
+}
+Example calls:
+remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1, 2));
+// Result: list(1, 2, 3, 4)
+remove_duplicates(list("a", "x", "b", "c", "c", "b", "d"));
+// Result: list("a", "x", "b", "c", "d")
