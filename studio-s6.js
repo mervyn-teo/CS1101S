ce99f29b682c1de69a485ba6285c06926365199f
Source Academy User <No public email provided>
2023-09-18 02:24:33 +0800

    Changes made from Source Academy

--- studio-s6.js
+++ studio-s6.js
@@ -19,3 +19,37 @@ display(remove_duplicates(list(1, 2, 3, 4, 4, 3, 2, 1, 2)));
 // Result: list(1, 2, 3, 4)
 remove_duplicates(list("a", "x", "b", "c", "c", "b", "d"));
 // Result: list("a", "x", "b", "c", "d")
+
+
+
+// Our friend Louis Reasoner has a pocket full of change. He wants to buy a snack that will
+// cost him x cents, and he wants to know all the ways in which he can use his change to
+// make up that amount. Please help him in writing a function which takes as parameters the
+// amount x and a list of all the coins Louis has in his pocket, and returns a list of lists, such
+// that each sub-list of the result contains a valid combination to make up x. A combination
+// may appear more than once, since it may be using different coins of the same denomination.
+// Help Louis by filling in the ellipses ... in his incomplete solution:
+function makeup_amount(x, coins) {
+    if (x === 0) {
+        return list(null);
+    } else if (x < 0 || is_null(coins)) {
+        return null;
+    } else {
+        // Combinations that do not use the head coin.
+        const combi_A = makeup_amount(x, tail(coins));
+        // Combinations that do not use the head coin
+        // for the remaining amount.
+        const combi_B = makeup_amount(x - head(coins), tail(coins));
+        // Combinations that use the head coin.
+        const combi_C = map(l => pair(head(coins), l), combi_B);
+        return append(combi_A, combi_C);
+    }
+}
+// Example call:
+makeup_amount(22, list(1, 10, 5, 20, 1, 5, 1, 50));
+// Result: list(list(20, 1, 1), list(10, 5, 1, 5, 1), list(1, 20, 1),
+// list(1, 20, 1), list(1, 10, 5, 5, 1),
+// list(1, 10, 5, 1, 5))
+
+// Note: The sublist list(1, 20, 1) appears twice. Each appearance of the number 1 refers
+// to a different coin.
