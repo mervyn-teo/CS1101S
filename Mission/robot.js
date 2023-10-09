d0ded7e654ab5869d121353ae90f5cb0c6a643af
Source Academy User <No public email provided>
2023-10-09 15:05:07 +0800

    Changes made from Source Academy

--- Mission/robot.js
+++ Mission/robot.js
@@ -1,19 +1,37 @@
 const motor_C = ev3_motorC();
 const motor_D = ev3_motorD();
-
-function turn_left(a){ //a is multiple of 90 degree
-    ev3_runToRelativePosition(motor_C, a * 250, 250);
-    ev3_runToRelativePosition(motor_D, a * -250, 250);
-}
-
-function turn_right(a){ //a is multiple of 90 degree
-    ev3_runToRelativePosition(motor_C, a * -250, 250);
-    ev3_runToRelativePosition(motor_D, a * 250, 250);
-}
-
-function run_forward(distance) {
-    ev3_runToRelativePosition(motorC, distance*17, 250);
-    ev3_runToRelativePosition(motorD, distance*17, 250);
+let buffer = 10;    //time buffer for ev3_pause function
+function turn_left(a){ //a is in degrees
+    ev3_runToRelativePosition(motor_C, a / 90 * 250, 250);
+    ev3_runToRelativePosition(motor_D, a / 90 * -250, 250);
     ev3_pause(5000);
 }
+
+function turn_right(a){ //a is in 90 degree
+    ev3_runToRelativePosition(motor_C, a / 90 * -250, 250);
+    ev3_runToRelativePosition(motor_D, a / 90 * 250, 250);
+    ev3_pause(5000);
+}
+
+function run_forward(distance) {//distance is in cm
+    ev3_runToRelativePosition(motor_C, distance*17, 250);
+    ev3_runToRelativePosition(motor_D, distance*17, 250);
+    ev3_pause(5000);
+}
+
+// Pt. 1
+ev3_speak("hello world");
+
+// Pt.2
+run_forward(10);
+
+// Pt.3
+turn_left(90);
+
+// Pt.4
+run_forward(10);
+turn_left(90);
+run_forward(5);
+turn_right(90);
+
 
