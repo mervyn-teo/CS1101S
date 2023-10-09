9ff1703557ca91211ff893ca8af5989f63647974
Source Academy User <No public email provided>
2023-10-09 14:03:30 +0800

    Changes made from Source Academy

--- robot.js
+++ robot.js
@@ -11,9 +11,9 @@ function turn_right(a){ //a is multiple of 90 degree
     ev3_runToRelativePosition(motor_D, a * 250, 250);
 }
 
-ev3_speak("turn_left");
-turn_left(1);
-ev3_pause(1000);
-ev3_speak("turn_right");
-turn_right(1);
-ev3_pause(1000);
+function run_forward(distance) {
+    ev3_runToRelativePosition(motorC, distance*17, 250);
+    ev3_runToRelativePosition(motorD, distance*17, 250);
+    ev3_pause(5000);
+}
+
