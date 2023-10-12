b3a6d496d381eedf7b7b5a1ab0e13c5c3336caa4
Source Academy User <No public email provided>
2023-10-12 13:06:04 +0800

    Changes made from Source Academy

--- Reflection/reflection r8.js
+++ Reflection/reflection r8.js
@@ -1,7 +1,12 @@
+let tries = 3;
 function make_withdraw(balance, password) {
     
     function withdraw(amount,pwd) {
-        if (pwd !== password) {
+        if (tries === 0){
+            return "Account disabled";
+        }
+        else if (pwd !== password) {
+            tries = tries - 1;
             return "Wrong password; no withdraw";
         }else if(balance >= amount) {
             balance = balance - amount;
@@ -15,4 +20,8 @@ function make_withdraw(balance, password) {
 
 const acc = make_withdraw(100, "my_password");
 acc(30, "his_passcode"); // returns "Wrong password; no withdraw"
-acc(30, "my_password"); // returns 70
+acc(30, "his_passcode"); // returns "Wrong password; no withdraw"
+acc(30, "his_passcode"); // returns "Wrong password; no withdraw"
+acc(30, "his_passcode"); // returns "Wrong password; no withdraw"
+acc(30, "my_password");
+
