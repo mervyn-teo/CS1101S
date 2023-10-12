0304225e59b1cef577b34b589d824fbc25939c0e
Source Academy User <No public email provided>
2023-10-12 13:07:46 +0800

    Changes made from Source Academy

--- Reflection/reflection r8.js
+++ Reflection/reflection r8.js
@@ -1,3 +1,5 @@
+// Qn.1
+
 let tries = 3;
 function make_withdraw(balance, password) {
     
@@ -25,3 +27,28 @@ acc(30, "his_passcode"); // returns "Wrong password; no withdraw"
 acc(30, "his_passcode"); // returns "Wrong password; no withdraw"
 acc(30, "my_password");
 
+// Qn.2
+
+let commission = 25; // my commission in dollars
+
+// return a calculator for total price
+// total price = (commission + cost) * (1 + tax_rate)
+
+function make_price_calculator(tax_rate) {
+    function calculator(cost) {
+        return (commission + cost) * (1 + tax_rate);
+    }
+    return calculator;
+}
+
+const calc = make_price_calculator(0.07);
+commission = 125;
+calc(75);
+
+// Qn.3
+
+function curry(f) {
+    return x => y => f(x, y);
+}
+
+(curry(math_pow))(3)(4);
