6c909027b83e799a0125d0dd12d4115128f02b8d
mervyn-teo <teomervyn@yahoo.com>
2023-10-13 15:46:35 +0800

    Update robot_2nd_misson.js
    Up and running

--- Mission/robot_2nd_misson.js
+++ Mission/robot_2nd_misson.js
@@ -1,6 +1,5 @@
-
-const ultra_sound = ev3_ultrasonicSensor();
-let multiplier = 5; // multiplier to calibrate ultrasound sensor
+const ultra_sound = ev3_ultrasonicSensor(2);
+let multiplier = 0.1*1.09*0.91743; // multiplier to calibrate ultrasound sensor
 const motor_C = ev3_motorC();
 const motor_D = ev3_motorD();
 
@@ -23,17 +22,17 @@ function distance_to_rev(distance) {
 function run_forward(distance) {
     ev3_runToRelativePosition(motor_C, distance_to_rev(distance), 250);
     ev3_runToRelativePosition(motor_D, distance_to_rev(distance), 250);
-    ev3_pause(distance_to_rev(distance) / 250 * 1000); // pause for runtime calculated using speed and distance
-    } //move forward, distance in cm
+    ev3_pause(math_abs(distance_to_rev(distance)) / 250 * 1000); // pause for runtime calculated using speed and distance
+    } //move forward, distance in mm
 
 function turn_left(a){ //a is multiple of 90 degree
     ev3_runToRelativePosition(motor_C, a*distance_to_rev(turn_circumference/4), 250);
     ev3_runToRelativePosition(motor_D, a*-distance_to_rev(turn_circumference/4), 250);
-    ev3_pause(a * -distance_to_rev(turn_circumference / 4) / 250 * 1000);
+    ev3_pause(math_abs(a) * distance_to_rev(turn_circumference / 4) / 250 * 1000);
 }//turn a * 90 degrees leftwards 
 
 function ultra_dist(){
-    return ev3_ultrasonicSensorDistance(ultra_sound * multiplier);
+    return ev3_ultrasonicSensorDistance(ultra_sound) * multiplier;
 }//the distance ultrasound sensor senses
 
 function check_obsticle(){//Check if the robot is facing obsticle
@@ -46,46 +45,52 @@ function check_obsticle(){//Check if the robot is facing obsticle
 }
 
 // Qn.1
-display(ev3_ultrasonicSensorDistance(ultra_sound * multiplier));
-
-// Qn.2
-function avoid(){
-    ev3_motorSetSpeed(motor_C, 250);
-    ev3_motorSetSpeed(motor_D, 250);
-    while (ultra_dist > 10) {
-    } 
-    ev3_motorSetStopAction(motor_C, "hold");
-    ev3_motorSetStopAction(motor_D, "hold");
-    run_forward(-30);
+// Displays the ultra_dist every second for 10s
+let i = 0;
+for(i = 0; i <= 10; i = i+ 1) {
+    display(ultra_dist());
+    ev3_pause(1000);
 }
+
+// Qn. 2
+function avoid(){
+    ev3_motorSetSpeed(motor_C, 100);
+    ev3_motorSetSpeed(motor_D, 100);
+    ev3_motorStart(motor_C);
+    ev3_motorStart(motor_D);
+    while (ultra_dist() > 10) {
+    } 
+    ev3_motorStop(motor_C);
+    ev3_motorStop(motor_D);
+    run_forward(-300);
+}
+
+avoid();
 
 // Qn.3
 function around(){
     ev3_motorSetSpeed(motor_C, 250);
     ev3_motorSetSpeed(motor_D, 250);
-    while (ultra_dist > 10) {
+    ev3_motorStart(motor_C);
+    ev3_motorStart(motor_D);
+    while (ultra_dist() > 10) {
     }
-    ev3_motorSetStopAction(motor_C, "hold");
-    ev3_motorSetStopAction(motor_D, "hold");
-    run_forward(-30);
+    ev3_motorStop(motor_C);
+    ev3_motorStop(motor_D);
+
+    //turn_left
     if (math_random() < 0.5){
         turn_left(1);
-        let is_blocked = true;
-        while (is_blocked){
-            run_forward(10);
-            turn_left(-1);
-            is_blocked = check_obsticle();
-        }
-        around();
+        run_forward(300);
+        turn_left(-1);
     } else{
         turn_left(-1);
-        let is_blocked = true;
-        while (is_blocked){
-            run_forward(10);
-            turn_left(1);
-            is_blocked = check_obsticle();
+        run_forward(300);
+        turn_left(1);
     }
     
+    run_forward(500);
     
 }
-}
+
+around();
