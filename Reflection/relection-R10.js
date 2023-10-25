304f802b5c4c28016cb2f06478f575bcd1cb57a8
Source Academy User <No public email provided>
2023-10-26 00:06:26 +0800

    Changes made from Source Academy

--- Reflection/relection-R10.js
+++ Reflection/relection-R10.js
@@ -1,1 +1,14 @@
-l
+function zip_list_of_streams(xs){
+    function helper(ys){
+        let tmp = undefined;
+        if (is_null(ys)){
+            set_head(ys, head(stream_tail(ys)));
+            return helper(ys);
+        } else{
+            tmp = head(ys);
+            set_head(ys, head(stream_tail(ys)));
+            return pair(tmp, () => helper(tail(ys)));
+        }
+    }
+    return helper(xs);
+}
