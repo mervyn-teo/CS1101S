4c6bce888ec95faa242abfefdf4302b6c8d188d3
Source Academy User <No public email provided>
2023-10-26 00:25:16 +0800

    Changes made from Source Academy

--- Reflection/relection-R10.js
+++ Reflection/relection-R10.js
@@ -12,3 +12,21 @@ function zip_list_of_streams(xs){
     }
     return helper(xs);
 }
+
+function add_streams(s1, s2) {
+    return is_null(s1)
+        ? s2
+        : is_null(s2)
+        ? s1
+        : pair(head(s1) + head(s2),
+                () => add_streams(stream_tail(s1),
+                                    stream_tail(s2)));
+}
+
+function partial_sum(s) {
+  return pair(head(s), () => add_streams(stream_tail(s), partial_sum(s)));
+}
+
+let a = enum_stream(0,100);
+display(eval_stream(a, 30));
+eval_stream(partial_sum(a),30);
