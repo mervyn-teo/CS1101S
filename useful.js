1a3a19da3363fb6a764cba3750fefe13786788f9
Source Academy User <No public email provided>
2023-11-04 02:15:54 +0800

    Changes made from Source Academy

---
+++ useful.js
@@ -0,0 +1,77 @@
+// QUESTION 2
+
+// SOLUTIONS
+
+//===============================================================
+// DO NOT REMOVE OR MODIFY THE FOLLOWING FUNCTIONS.
+// You may call them in your functions.
+//===============================================================
+function swap(A, i, j) {
+    const temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+//---------------------------------------------------------------
+function copy_array(A) {
+    const len = array_length(A);
+    const B = [];
+    for (let i = 0; i < len; i = i + 1) {
+        B[i] = A[i];
+    }
+    return B;
+}
+//---------------------------------------------------------------
+function reverse_array(A) {
+    const len = array_length(A);
+    const half_len = math_floor(len / 2);
+    for (let i = 0; i < half_len; i = i + 1) {
+        swap(A, i, len - 1 - i);
+    }
+}
+//---------------------------------------------------------------
+function array_to_list(A) {
+    const len = array_length(A);
+    let L = null;
+    for (let i = len - 1; i >= 0; i = i - 1) {
+        L = pair(A[i], L);
+    }
+    return L;
+}
+//---------------------------------------------------------------
+function list_to_array(L) {
+    const A = [];
+    let i = 0;
+    for (let p = L; !is_null(p); p = tail(p)) {
+        A[i] = head(p);
+        i = i + 1;
+    }
+    return A;
+}
+//---------------------------------------------------------------
+// Sorts the array of numbers in ascending order.
+function sort_ascending(A) {
+    const len = array_length(A);
+    for (let i = 1; i < len; i = i + 1) {
+        const x = A[i];
+        let j = i - 1;
+        while (j >= 0 && A[j] > x) {
+            A[j + 1] = A[j];
+            j = j - 1;
+        }
+        A[j + 1] = x;
+    }
+}
+//---------------------------------------------------------------
+function digits_to_string(digits) {
+    const len = array_length(digits);
+    let str = "";
+    for (let i = 0; i < len; i = i + 1) {
+        str = str + stringify(digits[i]);
+    }
+    return str;
+}
+// const D = [8, 3, 9, 2, 8, 1];
+// digits_to_string(D);  // returns "839281"
+//===============================================================
+// DO NOT REMOVE OR MODIFY THE ABOVE FUNCTIONS.
+//===============================================================
